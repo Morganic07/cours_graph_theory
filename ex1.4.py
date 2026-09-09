3674c0e61ed2a97676019842f499eeacd5de6c0e
Morgane <phoenixroyal78@gmail.com>
2026-09-09 10:38:52 +0200

    py

---
+++ ex1.4.py
@@ -0,0 +1,16 @@
+graph={0:[1,2],1:[0,2],2:[0,1]}
+
+#Each key represent a node, its value is the list of its neighbors
+
+def showStruct(dic):
+    nbNodes=len(dic) #nombre de clés
+    print("Nodes :")
+    for node in dic.keys(): #dic.keys() donne la liste des clés de dic, donc la liste des noeuds
+        print("v"+str(node),end=" ")
+    print("")    
+    print("Edges :")
+    for node in dic.keys():
+        for neighbor in dic[node]:
+            if (node < neighbor):
+                print("v"+str(node)+"--- v"+str(neighbor))
+showStruct(graph)    
