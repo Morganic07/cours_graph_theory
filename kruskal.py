3edd10a8630d38b9a40f148feb3914305399d28c
Morgane <phoenixroyal78@gmail.com>
2026-09-24 16:57:20 +0200

    kruskal

--- kruskal.py
+++ kruskal.py
@@ -8,3 +8,38 @@ def find_parent(parent, i):
     return find_parent(parent, parent[i])
 
 
+def union(parent, rank, x, y):
+    xroot = find_parent(parent, x)
+    yroot = find_parent(parent, y)
+
+    if rank[xroot] < rank[yroot]:
+        parent[xroot] = yroot
+    elif rank[xroot] > rank[yroot]:
+        parent[yroot] = xroot
+    else:
+        parent[yroot] = xroot
+        rank[xroot] += 1
+
+
+
+def kruskal(graph, V):
+    result = []
+    i = 0
+    e = 0
+    graph = sorted(graph, key=lambda item: item[2])
+    parent = [i for i in range(V)]
+    rank = [0] * V
+
+    while e < V - 1:
+        u, v, w = graph[i]
+        i = i + 1
+        x = find_parent(parent, u)
+        y = find_parent(parent, v)
+
+        if x != y:
+            e = e + 1
+            result.append([u, v, w])
+            union(parent, rank, x, y)
+    
+    return result 
+
